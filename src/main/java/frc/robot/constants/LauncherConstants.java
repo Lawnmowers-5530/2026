b8f5f5d17d78b514573f89edcbff368019a6583a
Hugo Wang <hwang@greenhillsschool.org>
2026-01-26 19:22:05 -0500

    cpp native module: java side

---
+++ src/main/java/frc/robot/constants/LauncherConstants.java
@@ -0,0 +1,8 @@
+package frc.robot.constants;
+
+public class LauncherConstants { //TODO: fill in constants
+    static final int canId = 0;
+    static final double kI = 0.0;
+    static final double kA = 0.0;
+    static final double kV = 0.0;
+}
