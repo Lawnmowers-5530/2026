0b5b7d8d0f2c4e82c780a20a3bc7a225fda66d57
Hugo Wang <hwang@greenhillsschool.org>
2026-02-14 12:10:45 -0500

    Add comment for merge conflict resolution example
    Added a comment to create a merge conflict example.

--- src/main/java/frc/robot/constants/LauncherConstants.java
+++ src/main/java/frc/robot/constants/LauncherConstants.java
@@ -5,7 +5,7 @@ public class LauncherConstants { //TODO: fill in constants
     static final double kI = 0.0;
     static final double kA = 0.0;
     static final double kV = 0.0;
-
+    // create a merge conflict so Lawncoder knows how to resolve conflicts
     public static final double motorToYawRot = 8;
     public static final double motorToPitchRot = 17;
     public static final double motorToFlywheelRot = 12/18;
