928bfb241d11f47d927c1bb465fc5adeca446828
nickroney <nroney101@gmail.com>
2026-03-07 07:27:06 -0500

    less ambiguous constants

--- src/main/java/frc/robot/constants/VisionConstants.java
+++ src/main/java/frc/robot/constants/VisionConstants.java
@@ -17,9 +17,9 @@ import edu.wpi.first.units.Unit;
 public class VisionConstants {
         public static final String kCameraName = "main";
         // Cam mounted facing forward, half a meter forward of center, half a meter up from center.
-        public static final Transform3d kRobotToCam1 =
+        public static final Transform3d kRobotToCamBack =
                 new Transform3d(new Translation3d( Units.inchesToMeters(-27.5/2), Units.inchesToMeters(27/2 - 8), Units.inchesToMeters(7.5)), new Rotation3d(Math.toRadians(5), Math.toRadians(45), Math.toRadians(180)));
-        public static final Transform3d kRobotToCam2 = 
+        public static final Transform3d kRobotToCamLeft = //TODO
         new Transform3d(new Translation3d(Units.inchesToMeters(27.5/2 - 3),Units.inchesToMeters(26.5/2),Units.inchesToMeters(8.5)), new Rotation3d(Math.toRadians(16.5), Math.toRadians(30), Math.toRadians(0)));
         // The layout of the AprilTags on the field
         public static final AprilTagFieldLayout kTagLayout =
